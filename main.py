acc76f5551719ad5c5aef4650aa1a6f9ebec82bf
ZanoniCFernandes <zanoni.c.fernandes@gmail.com>
2026-05-30 17:14:15 -0300

    feat: possibilita input entrada execucao e adiciona blocos de validacao

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from transpiler import transpile
+import sys
 
-with open("input.swhthon", "r", encoding="utf-8") as f:
+try:
+  file_path = sys.argv[1]
+except:
+  print("Um programa fonte de entrada deve ser inserido. Encerrando transpilador")
+  sys.exit(1)
+
+try:
+  with open(file_path, "r", encoding="utf-8") as f:
     code = f.read()
+except:
+  print("Erro ao abrir arquivo de entrada. Encerrando transpilador")
+  sys.exit(1)
 
-output = transpile(code)
+try:
+  output = transpile(code)
+except:
+  print("Erro ao transpilar arquivo fonte swhthon. Encerrando transpilador")
+  sys.exit(1)
 
-with open("output.py", "w", encoding="utf-8") as f:
-    f.write(output)
+try:
+  with open("output.py", "w", encoding="utf-8") as f:
+      f.write(output)
+except:
+  print("Erro ao gerar arquivo de saída em python. Encerrando transpilador")
+  sys.exit(1)
 
 print("[!] O código foi transpilado com sucesso! ")
 print("[>] O arquivo 'output.py' foi gerado nesta pasta. ")
