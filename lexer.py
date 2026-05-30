d89716296d17b7d10a3028549f2c30709fa59ae8
ZanoniCFernandes <zanoni.c.fernandes@gmail.com>
2026-05-30 19:31:55 -0300

    refactor: isola compilação do regex para acelerar processamento

--- lexer.py
+++ lexer.py
@@ -1,28 +1,36 @@
 import re
-from lexer_tokens import TOKENS
+from lexer_tokens import TOKEN_SPECS, KEYWORDS
 
 class Lexer:
 
-  def __init__(self, tokens_list=None):
-    self.tokens_list = tokens_list or TOKENS
+  def __init__(self, token_specs=None, keywords=None):
+      self.token_specs = token_specs or TOKEN_SPECS
+      self.keywords = keywords or KEYWORDS
+      self.rules = [
+          (name, re.compile(pattern))
+          for name, pattern in self.token_specs
+      ]
 
   def tokenize(self, code):
       generated_tokens = []
 
       while code:
           match = None
-          for token_type, pattern in self.tokens_list:
-              regex = re.compile(pattern)
+          for token_type, regex in self.rules:
               match = regex.match(code)
+              
+              if not match:
+                continue
+              
+              text = match.group(0)
 
-              if match:
-                  text = match.group(0)
-
-                  if token_type != 'SKIP':
-                      generated_tokens.append((token_type, text))
-
-                  code = code[len(text):]
-                  break
+              if token_type == 'ID' and text in self.keywords:
+                    token_type = self.keywords[text]
+              
+              if token_type != 'SKIP':
+                    generated_tokens.append((token_type, text))
+              code = code[len(text):]
+              break
 
           if not match:
               raise SyntaxError("Token é incorreto")
