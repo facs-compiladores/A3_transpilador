21155f735895cce2b521bfa1e4bdb43cfad15573
ZanoniCFernandes <zanoni.c.fernandes@gmail.com>
2026-05-30 19:31:07 -0300

    refactor: isola keywords de specs em lexer_tokens

--- lexer_tokens.py
+++ lexer_tokens.py
@@ -1,49 +1,37 @@
-TOKENS = [
-    # Palavras reservadas
-    ('PROGRAM', r'programu'),
-    ('END', r'mwisho'),
-    ('INT', r'nambari'),
-    ('FLOAT', r'kuelea'),
-    ('STRING', r'kamba'),
-    # ('BOOLEAN', r'kweliuongo'), 
-    ('BOOLEAN_TRUE', r'kweli'),
-    ('BOOLEAN_FALSE', r'uongo'),
-    ('IF', r'ikiwa'),
-    ('ELSE', r'mwingine'),
-    ('WHILE', r'wakati'),
-    ('FOR', r'kwa'),
-    ('INPUT', r'pembejeo'),
-    ('PRINT', r'chapa'),
-
-    ('ASSIGN', r':='),
+TOKEN_SPECS = [
     ('NUMBER', r'\d+(\.\d+)?'),
-    ('ID', r'[a-zA-Z][a-zA-Z0-9]*'),
     ('TEXT', r'"[^"]*"'),
-
-    # Operações básicas
+    ('ASSIGN', r':='),
     ('PLUS', r'\+'),
     ('MINUS', r'-'),
     ('MULT', r'\*'),
     ('DIV', r'/'),
-
-    # Maior que, menor que, igual a...
-    ('LT', r'<'), 
+    ('LT', r'<'),
     ('GT', r'>'),
     ('EQ', r'=='),
-
-    # Simbolos matemáticos
     ('LPAREN', r'\('),
     ('RPAREN', r'\)'),
     ('LBRACE', r'\{'),
     ('RBRACE', r'\}'),
-    ('LBRACK', r'\['),
-    ('RBRACK', r'\]'),
-
-    # . | , e ;
     ('DOT', r'\.'),
     ('COMMA', r','),
     ('SEMIC', r';'),
-
-    # Para ignorar os caracteres não-importantes
     ('SKIP', r'[ \t\n]+'),
+    ('ID', r'[A-Za-z][A-Za-z0-9]*'),
 ]
+
+KEYWORDS = {
+    'programu': 'PROGRAM',
+    'mwisho': 'END',
+    'nambari': 'INT',
+    'kuelea': 'FLOAT',
+    'kamba': 'STRING',
+    'kweli': 'BOOLEAN_TRUE',
+    'uongo': 'BOOLEAN_FALSE',
+    'ikiwa': 'IF',
+    'mwingine': 'ELSE',
+    'wakati': 'WHILE',
+    'kwa': 'FOR',
+    'pembejeo': 'INPUT',
+    'chapa': 'PRINT',
+}
