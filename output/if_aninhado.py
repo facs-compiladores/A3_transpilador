cf81f5ebfe19ea984564e513d870e4eac8a85581
Marcelo <19808040+mmcav@users.noreply.github.com>
2026-06-10 20:16:17 -0300

    test: adiciona exemplo de bloco aninhado

---
+++ output/if_aninhado.py
@@ -0,0 +1,15 @@
+a = 0
+b = 0
+c = 0
+d = 0
+a = 10
+b = 5
+c = 20
+d = 15
+print("===# TESTE CONDICIONAL ANINHADO #===")
+if (a > b):
+    print("A maior que B")
+    if (c > d):
+        print("C maior que D")
+else:
+    print("A nao e maior que B")
